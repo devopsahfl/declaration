8fa446da4e629862e742f2e43e51fb3064efed67
devopsahfl <nikhil.bhagat@aadharhousing.com>
2022-04-14 11:01:39 +0530

    Responsive

--- resources/views/dynamic_dependent.blade.php
+++ resources/views/dynamic_dependent.blade.php
@@ -2,74 +2,109 @@
 <!DOCTYPE html>
 <head>
     <title>Declaration Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <style type="text/css">
-   .box{
-    width:600px;
-    margin:0 auto;
-    border:1px solid blue;
-   }
-   h3{
-    text-align:center;
-    color:black;
-    font-size: 40px;
-   }
-   .btn-primary{
-    background-color:#C63737;
-   }
-   .row{
-    display: flex;
-  justify-content: center;
-  align-items: center;
-   }
-   body{
-    background-image: url('files/dhachik.jpg');
-    background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-size: cover;
-   }
+    .leftside, .rightside{
+        height:50vh;
+        width:100%;
+    }
+
+    @media screen and (min-width:768px) 
+    {
+      .leftside, .rightside{
+        height:100vh;
+    }
+      
+    }
+
+
+    .leftside{
+      /* background: red; */
+      
+      background:url("files/dhachik.jpg") no-repeat fixed center;
+    }
+    .rightside{
+      /* background:blue; */
+      background:url("files/white.png") no-repeat fixed center;
+    }
   </style>
 </head>
-<body>
-<div class="logo" style="padding-top:5px;padding-left:70px;padding-bottom:3px;">
+<body >
+<!-- <div class="logo" style="padding-top:5px;padding-left:70px;padding-bottom:3px;">
     <div class="form-inline">
     <a href="{{url('/')}}"><img  src="files/logo.png"></a>
     </div>
-</div>
-  <form action="sendOtp" method="POST">
-<div class="container box p-4 ml-2 border border-sucess" style=" margin-top: 30px!important;">
-<textarea style="display:none;" id="master" name="master">{{$country_list}}</textarea>
+</div> -->
+<style>
+  
+@media screen and (max-width: 992px) {
+  .mainbar {
    
-   <h3><b>Channel Partner Details</b></h3><br />
-   <div class="form-group">
-     <label for="type">TYPE</label>
-    <select name="type" id="type" class="form-control input-lg dynamic" data-dependent="partner_code" onChange="getpartner_code()">
-     <option disabled="disabled" value="Selected">Select Type</option>
+  }
+}
+  </style>
+ <div class="container  p-4 ml-2  border-sucess" style=" margin-top: 10px; justify-content:center;">
+                  <textarea style="display:none;" id="master" name="master">{{$country_list}}</textarea>
 
-    </select>
-   </div>
-   <br />
-   <div class="form-group">
-     <label for="partner_code">PARTNER CODE</label>
-    <select name="partner_code" id="partner_code" class="form-control input-lg dynamic" data-dependent="contact_number" onChange="getContact_number()">
-    </select>
-   </div>
-   <br />
-   <!-- <div class="form-group">
-    <select  type ="text" name="contact_number" id="contact_number" class="form-control input-lg"  aria-expanded="false" readonly></select>
-   </div> -->
-   <label for="contact_number"> MOBILE NUMBER</label>
-   <input  type ="text" name="contact_number" id="contact_number" class="form-control input-lg"></select><br>
-   <div class="row">
-   <button type="submit" id="sendotp" class="btn btn-primary">Send OTP</button>
+  <form action="sendOtp" method="POST">
+<div class="row no-gutters">
+  <div class="col-lg-6 col-md-12 no-gutters">
+    <div class="leftside">
+
+    </div>
   </div>
-   {{ csrf_field() }}
-   <br />
-   <br />
+
+  <div class="col-lg-6 col-md-12 no-gutters">
+    <div class="rightside d-flex justify-content-center align-items-center">
+      <div class="row-lg-12 row-md-12 row-md-12">
+
+                    <div class="mainbar">
+                        <h3 style="margin-left:50px;" ><b>Channel Partner Details</b></h3><br />
+                    </div>
+            <div class="col-lg-6 col-md-12 col-sm-12">
+                        <div class="form-group">
+                          
+                          <label for="type">TYPE</label>
+                          <select name="type" id="type" class="form-control input-lg dynamic" data-dependent="partner_code" onChange="getpartner_code()">
+                          <option disabled="disabled" value="Selected">Select Type</option>
+
+                          </select>
+            </div>
+                    <br />
+            <div class="form-group">
+                      <label for="partner_code">PARTNER CODE</label>
+                      <select name="partner_code" id="partner_code" class="form-control input-lg dynamic" data-dependent="contact_number" onChange="getContact_number()">
+                      </select>
+            </div>
+                    <br />
+                    <!-- <div class="form-group">
+                      <select  type ="text" name="contact_number" id="contact_number" class="form-control input-lg"  aria-expanded="false" readonly></select>
+                    </div> -->
+            <div class="form-group">
+                    <label for="contact_number"> MOBILE NUMBER</label>
+                    <input  type ="text" name="contact_number" id="contact_number" class="form-control input-lg"></select><br>
+                    
+                    <button type="submit" id="sendotp" class="btn btn-primary">Send OTP</button>
+            </div>
+                    {{ csrf_field() }}
+                    <br />
+                    <br />
+          
+          </div>
+
+      </div>         
+    </div>
   </div>
+
+</div>
   </form>
+  
+  
+
 </body>
 </html>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
