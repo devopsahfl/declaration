64d49d790ea104f1ecb0a0308d96a26b943205b2
devopsahfl <nikhil.bhagat@aadharhousing.com>
2022-04-15 13:50:17 +0530

    Final Changes

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,6 +1,6 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,97 +9,110 @@
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.min.css" integrity="sha512-hAFMASi3RewTdcR5m7meVmbFAwEu+2t9oXGrckvHgW5ozmKpk7AIfOM9Y/DfdKvfVMTZB6cwXpCBPeIyaCqT2Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script> -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script> -->
 
 
     <style>
-            .container{
-                padding:30px;
-                
-                
+        .container {
+            padding: 30px;
 
-            }
-            h3{
-                text-align:center;
-                color: Black;
-            }
-            .row{
-                padding-right: 400px;
-                padding-left:  400px;
-            }
-            .col{
-                position: relative;
-                left: 150px;
-                color:black;
-            
-            }
-            .col-1{
-                display: flex;
-                justify-content: center; 
-            }
-            
-            .btn:hover {
+
+
+        }
+
+        h3 {
+            text-align: center;
+            color: Black;
+        }
+
+        .row {
+            padding-right: 400px;
+            padding-left: 400px;
+        }
+
+        .col {
+            position: relative;
+            left: 150px;
+            color: black;
+
+        }
+
+        .col-1 {
+            display: flex;
+            justify-content: center;
+        }
+
+        .btn:hover {
             background-color: #008CBA;
             color: white;
-            }
-        
-            .details{
-                margin-top:40px;
-            margin-left: 100px;
-            }
-            body{
-                background-image: url('files/dhachik.jpg');
-                background-repeat: no-repeat;
-                 background-attachment: fixed;
-                 background-size: cover;
-   }
+        }
+
+        .details {
+            margin-top: 40px;
+            margin-left: 70px;
+        }
+
+        body {
+            background-image: url('files/dhachik.jpg');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+
+        .terms-cond {
+            text-align: center;
+            padding: 20px;
+        }
     </style>
 </head>
+
 <body>
     <h3><b><u>Code of Conduct for Aadhar Mitra (AM), Mahila Aadhar Mitra (MAM) and Direct Selling Agent (DSA)</u></b></h3>
 
 
-     <div class="details">
-     <h4><b>TYPE:-</b> {{$type}}</h4>
-     <h4><b>PARTNER CODE:-</b> {{$partner_code}}</h4>
-     <h4><b>MOBILE NUMBER:-</b> {{$mobileNumber}}</h4>
-        </div>
+    <div class="details">
+        <h4><b>TYPE:-</b> {{$type}}</h4>
+        <h4><b>PARTNER CODE:-</b> {{$partner_code}}</h4>
+        <h4><b>MOBILE NUMBER:-</b> {{$mobileNumber}}</h4>
+    </div>
 
 
 
     <form action="save" method="POST">
-    @csrf
-    <div class="container">
-        <embed src="files/codeOfConduct.pdf" type="application/pdf" width="100%" height="600px">
-    </div>
-            <!-- <div class="row"> -->
-            <div class="col">
-         
-                <p><input type="checkbox" id="checkbox" required name="terms"><b>I hereby<u> declare and confirm that I have read, understood and agree to abide by the DSA Code of Conduct, Anti-Corruption, Anti Money Laundering,<p> KYC and social media Policy of Aadhar Housing Finance Ltd. I further confirm that the same has been explained to my employees.<p></u></p></b>
-                        
-            </div></br>
-                <div class="col-1">
-                        <button type="submit" id="submit" name="submit" class="btn btn-primary">Submit</button>
-                </div></br></br>
-        <!-- </div> -->
-        </form>
+        @csrf
+        <div class="container">
+            <embed src="files/codeOfConduct.pdf" type="application/pdf" width="100%" height="600px">
+        </div>
+        <!-- <div class="row"> -->
+        <div class="terms-cond">
 
-        <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js" 
+            <p><input type="checkbox" id="checkbox" required name="terms"><b>I hereby<u> declare and confirm that I have read, understood and agree to abide by the DSA Code of Conduct, Anti-Corruption, Anti Money Laundering,<p> KYC and social media Policy of Aadhar Housing Finance Ltd. I further confirm that the same has been explained to my employees.
+                        <p>
+                    </u></p></b>
+
+        </div></br>
+        <div class="col-1">
+            <button type="submit" id="submit" name="submit" class="btn btn-primary">Submit</button>
+        </div></br></br>
+        <!-- </div> -->
+    </form>
+
+    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js" 
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
          crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.min.js" 
         integrity="sha512-ENqEDq2o2Ho8dh8tkYjoJ8m7Xvyz8f1H8OCD2ub4b3HfZAxD+TahCpJombeLtO6+E9nLRFmktCN2LcroxZj3hg==" 
         crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
-        <!-- <script>
+    <!-- <script>
         $('#submit').on('click', function(){
             console.log("submit");
             var data=$('#checkbox').val();
@@ -118,4 +131,5 @@
         })
     </script> -->
 </body>
+
 </html>
--- resources/views/verify.blade.php
+++ resources/views/verify.blade.php
@@ -1,9 +1,7 @@
-
 @if(Session::has('warning'))
 <script>
-swal("Invalid OTP!", "Ensure that you have entered correct OTP!", "warning")
-});
-
+  swal("Invalid OTP!", "Ensure that you have entered correct OTP!", "warning")
+  });
 </script>
 @endif
 
@@ -13,10 +11,10 @@ swal("Invalid OTP!", "Ensure that you have entered correct OTP!", "warning")
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous"></script>
 <script>
-swal("Invalid OTP!", "Ensure that you have entered correct OTP!", "warning");
-
+  swal("Invalid OTP!", "Ensure that you have entered correct OTP!", "warning");
 </script>
 
 <!-- <script>
@@ -25,53 +23,65 @@ swal("Successfully Sent!", "OTP sent on your registered mobile number", "success
 </script> -->
 
 <style>
+  .center {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    justify-content: center;
+    align-items: center;
+    max-width: 100%;
+  }
 
-.center{
-  position: absolute;
-  top:50%;
-  left: 50%;
-  transform: translate(-50% , -50%);
-  justify-content:center;
-  align-items:center;
-}
+  body {
+    background-image: url('files/dhachik.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+  }
 
-body{
-                background-image: url('files/dhachik.jpg');
-                background-repeat: no-repeat;
-                 background-attachment: fixed;
-                 background-size: cover;
-   }
-   .btn-outline-primary{
+  /* .btn-outline-primary{
     background-color:green;
-   }
+   } */
+  @media only screen and (max-width: 600px) {
+    .form-control {
+      width: 300px;
+      height: 45px;
+    }
+
+    .submit-wrap {
+      justify-content: center;
+    }
+  }
 </style>
- <div class="center p-4 ml-2 border border-sucess">
+<div class="center justify-content-center text-align-center">
 
-<form action="verifyOtp" method="POST">
-                  @csrf
+  <form action="verifyOtp" method="POST">
+    @csrf
 
-                  <div class=" row justify-content-center">
-                  <input type="text"  id="codeotp"name="codeotp" class="invisible" value={{$otp}} >
-                </div>
-                <div  class="flex">
-					        <div class="enter_otp"></div>
-					        <h1 class="text-danger text-center">ENTER OTP</h1>
-                <div class="row">
-                <br>
-                  <input type="text"  id="userotp"name="userotp" class="form-control border border-danger shadow" style="align-item:center" maxlength="6" required >
-                </div>
-<br>
-             <div class="row  justify-content-center">
-                 <br>
-                <button type="submit" class="btn btn-outline-primary" style="width:250px;align-items:center;text-align:center ;color:white;" onClick="function()">Submit</button>
-            </div>
+    <div class=" row" style="text-size-adjust:auto;">
+      <input type="text" id="codeotp" name="codeotp" class="invisible" value={{$otp}}>
+    </div>
+
+    <div class="row justify-content-center">
+      <h1 for="enterOtp" style="text-align-center">ENTER OTP</h1>
+    </div>
+    <br>
+
+    <div class="row">
+      <input type="text" id="userotp" name="userotp" style="text-align:center" class="form-control border border-danger shadow" maxlength="6" required style="width:500px;align-items:center;">
+    </div>
+    <br>
+
+    <div class="row submit-wrap">
+      <br>
+      <button type="submit" class="btn btn-outline-primary" style="width:250px;align-items:center;text-align:center;" onClick="function()">Submit</button>
+    </div>
 
 
-</div>
-             
 
-            </div>
-</form>
+
+  </form>
 
 </div>
 <script>
